b0a5ad9af8c08f3e1c34258e6a5057f0c5376c3a
AKSHAY <AKSHAY@LAPTOP-AUE2HLHR>
2023-02-25 20:23:34 +0530

    {akshay}: change code

---
+++ src/Github/github.java
@@ -0,0 +1,12 @@
+package Github;
+
+public class github {
+
+public static void main(String[] args) {
+	System.out.println("hi");
+	System.out.println("hello");
+	System.out.println("getdown");
+	System.out.println("Rahul");
+}
+
+}
