047b668ec5894188fa3fa046847d9eaa4b9819a2
AKSHAY <AKSHAY@LAPTOP-AUE2HLHR>
2023-02-26 00:27:17 +0530

    [akshay] hi code changed

---
+++ src/Github/akshay.java
@@ -0,0 +1,8 @@
+package Github;
+
+public class akshay 
+{
+public static void main(String[] args) {
+	System.out.println("lllllllllllllllllllllllllove");
+}
+}
---
+++ src/Github/new_new.java
@@ -0,0 +1,8 @@
+package Github;
+
+public class new_new 
+{
+public static void main(String[] args) {
+	System.out.println("byb bye");
+}
+}
